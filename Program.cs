1df63e100cc04cffeb47bb09be0cdcf1d40714ff
Alex <mr.tal44@yandex.ru>
2022-12-25 14:22:13 +0300

    Решение

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿void PrintArray(string[] words)
+
+{
+    int size = words.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{words[i]}  ");
+    }
+}
+
+
+
+void FinalTask3month()
+{
+    string[] words = new string[] { "hello", "2", "world", ":-)" };
+    int lengthWords = words.Length;
+
+    string[] newwords = new string[lengthWords];
+    int j = 0;
+    for (int i = 0; i < lengthWords; i++)
+    {
+        int count = words[i].Length;
+        if (count <= 3)
+        {
+            newwords[j] = words[i];
+            j++;
+
+        }
+    }
+    lengthWords = j;
+
+    PrintArray(words);
+    Console.WriteLine();
+    Console.WriteLine();
+    PrintArray(newwords);
+}
+
+
+
+FinalTask3month();
